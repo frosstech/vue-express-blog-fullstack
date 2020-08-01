9e0a61dc2c5c526b2e8d5ce2c32ae4ce713e6a6f
Akash AJ <frosstech@gmail.com>
2020-08-01 13:27:07 +0530

    Second phase error resolved

--- server/index.js
+++ server/index.js
@@ -11,13 +11,6 @@ const posts = require('./routes/api/posts')
 
 app.use('/api/posts', posts)
 
-app.use(express.static(__dirname + '/public/'))
-
-app.get(/.*/, (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
-
-
 if(process.env.NODE_ENV == 'production') {
     app.use(express.static(__dirname + '/public/'))
 
